f03161b6e6f8bf9c67148dd8b27a148284a3070a
JLW1808 <jacob.woolbright1234@gmail.com>
2025-03-18 19:45:41 -0500

    Remove recipe from file helper test

--- src/test/java/pro/pantrypilot/helpers/FileHelperTest.java
+++ src/test/java/pro/pantrypilot/helpers/FileHelperTest.java
@@ -13,7 +13,6 @@ class FileHelperTest {
         assertNotNull(FileHelper.readFile("static/index.html"), "index.html should not be null");
         assertNotNull(FileHelper.readFile("static/lists/list.html"), "list.html should not be null");
         assertNotNull(FileHelper.readFile("static/lists/lists.html"), "lists.html should not be null");
-        assertNotNull(FileHelper.readFile("static/recipes/recipe.html"), "recipe.html should not be null");
         assertNotNull(FileHelper.readFile("static/recipes/recipes.html"), "recipes.html should not be null");
         assertNotNull(FileHelper.readFile("static/login/login.html"), "login.html should not be null");
         assertNotNull(FileHelper.readFile("static/signup/signup.html"), "signup.html should not be null");
